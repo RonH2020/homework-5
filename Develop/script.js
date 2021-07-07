4df1106b4154c0602b969bffe1df84fdefbe5bdc
Ronald Holden <rholden.rh@gmail.com>
2021-07-07 13:23:47 -0500

    homework-5 files

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,4 @@
+// This will show the time in the jumbotron
 var timeStamp = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");;
 $("#currentDay").text(timeStamp);
 console.log(timeStamp);
@@ -6,3 +7,29 @@ var dateTime = document.getElementById("currentDay");
 
 var timeNow = Date.now();
 dateTime.innerHTML = timeStamp;
+
+// This will show JUST the time in the beginning of the input box
+var timeStamp = moment().format("h a");
+$("#timeRow").text(timeStamp);
+
+console.log(timeStamp);
+
+var dateTime = document.getElementById("timeRow");
+
+var timeNow = Date.now();
+dateTime.innerHTML = timeStamp;
+
+// This will be the loop to show the increments of the time through out the day
+for (let i = 0; i < 60; i++) {
+    dateTime[i];
+}
+
+
+// Click button to submit the input data by user
+
+
+$("#showInput").click(function() {
+    let input = document.getElementById("showInput").value
+    $(this).text(input);
+    console.log()
+  });
