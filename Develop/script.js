a5ab341871741b0e67161f70a338beb98b1c8aa9
Ronald Holden <rholden.rh@gmail.com>
2021-07-12 18:19:59 -0500

    homework-5 files

--- Develop/script.js
+++ Develop/script.js
@@ -1,40 +1,37 @@
 // This will show the time in the jumbotron
-var timeStamp = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");;
+var timeStamp = moment().format("dddd, MMMM Do YYYY, h:mm a");;
 $("#currentDay").text(timeStamp);
-console.log(timeStamp);
 
 var dateTime = document.getElementById("currentDay");
 
 var timeNow = Date.now();
 dateTime.innerHTML = timeStamp;
 
-// This will show JUST the time in the beginning of the input box
-var timeStamp = moment().format("h a");
-$("#timeRow").text(timeStamp);
 
-console.log(timeStamp);
 
-var dateTime = document.getElementById("timeRow");
-
-var timeNow = Date.now();
-dateTime.innerHTML = timeStamp;
-
-// This will be the loop to show the increments of the time through out the day
-for (let i = 0; i < 60; i++) {
-    dateTime[i];
-}
 
 
 // Click button to submit the input data by user
-var userInput = localStorage.getItem("text");
-var input = $('#text').val();
-console.log(userInput)
-console.log(input)
-$('#text').val();
+$(".saveBtn").on("click", function(event){
+    var userInput = localStorage.getItem("#textarea");
+    console.log(userInput)
+})
 
-$("#showInput").click(function() {
-    let input = document.getElementById("text").value
-    console.log(input)
-    localStorage.setItem("text", input);
 
-  });
+
+//   $("#showInput").on("click", function() {
+//       var userInput = localStorage.getItem("");
+//       var input = $('#text').val();
+//       console.log(userInput)
+//      // console.log(input)
+//      $('#text').val();
+//      $("text").text(userInput);
+
+//        $("#showInput").click(function() {
+//            let input = document.getElementById("text").value
+//            console.log(input)
+//             localStorage.setItem("text",  input);
+//             $("#9").val(localStorage.getItem(userInput));
+//        });
+//   });
+
